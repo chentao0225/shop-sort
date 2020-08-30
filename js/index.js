19f571dacd90e60fdc00c07fceb37212f541f8fb
chentao <chentao980225@163.com>
2020-08-30 22:05:07 +0800

    model

--- js/index.js
+++ js/index.js
@@ -1,61 +1,76 @@
-~(function () {
-  //原生ajax
+let shopModel = (function ($) {
+  let _data = null,
+    htmlStr = "",
+    $cardList = $(".cardList"),
+    $navList = $(".navbar-nav li"),
+    $cards = null;
+  function queryData() {
+    $.ajax({
+      url: "../json/product.json",
+      method: "get",
+      async: false,
+      success: (res) => {
+        _data = res;
+      },
+    });
+  }
 
-  let data = null,
-    xhr = new XMLHttpRequest();
-  xhr.open("get", "../json/product.json", false);
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      data = xhr.responseText;
-    }
-  };
-  xhr.send();
-  data = JSON.parse(data);
-  //   console.log(data);
-  let htmlStr = ``;
-  data.forEach(({ hot, img, price, time, title }) => {
-    htmlStr += `
-<div class="card" data-hot="${hot}" data-price="${price}" data-time="${time}">
-        <img src="${img}" class="card-img-top" alt="..." />
+  function createCards() {
+    if (_data.length === 0) return;
+    _data.forEach((item) => {
+      let $this = $(this);
+      let { price, hot, time, title, img } = item;
+      htmlStr += `
+      <div class="card"
+        data-price=${price}
+        data-hot=${hot}
+        data-time=${time}
+    >
+        <img src=${img} class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">
-          ${title}
-          </h5>
-          <p class="card-text">
-          价格：￥${price}
-          </p>
-          <p class="card-text">
-            好评：${hot}
-          </p>
-          <p class="card-text">
-            上架时间：${time}
-          </p>
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">价格：${price}</p>
+            <p class="card-text">热度：${hot}</p>
+            <p class="card-text"><small class="text-muted">${time}</small></p>
         </div>
-      </div>
-  `;
-  });
-  let cardList = document.querySelector(".cardList");
-  cardList.innerHTML = htmlStr;
-  let cards = document.querySelectorAll(".card");
-  let navList = document.querySelectorAll(".navbar-nav li");
-  navList.forEach((item) => {
-    item["flag"] = -1;
-    item.onclick = function () {
-      [].forEach.call(navList, (item) =>
-        item === this ? (this["flag"] *= -1) : (item["flag"] = -1)
-      );
-      let pai = this.getAttribute("data-pai");
-      cards = [].slice.call(cards, 0);
-      cards.sort((a, b) => {
-        a = a.getAttribute(pai);
-        b = b.getAttribute(pai);
+    </div>
+      `;
+      $cardList.html(htmlStr);
+    });
+  }
+
+  function shopSort() {
+    $cards = $(".card");
+    $navList.attr("flag", -1);
+    $navList.click(function () {
+      let _this = $(this);
+      _this
+        .attr("flag", _this.attr("flag") * -1)
+        .siblings()
+        .attr("flag", -1);
+      let pai = _this.attr("data-pai");
+      // console.log(pai);
+      $cards.sort((a, b) => {
+        // console.log(a, b);
+        let $a = $(a),
+          $b = $(b);
+        $a = $a.attr(pai);
+        $b = $b.attr(pai);
         if (pai === "data-time") {
-          a = a.replace(/-/g, "");
-          b = b.replace(/-/g, "");
+          $a = $a.replace(/-/g, "");
+          $b = $b.replace(/-/g, "");
         }
-        return (a - b) * this["flag"];
+        return ($a - $b) * _this.attr("flag");
       });
-      cards.forEach((item) => cardList.appendChild(item));
-    };
-  });
-})();
+      $cards.each((index, item) => $cardList.append(item));
+    });
+  }
+  return {
+    init() {
+      queryData();
+      createCards();
+      shopSort();
+    },
+  };
+})(jQuery);
+shopModel.init();
